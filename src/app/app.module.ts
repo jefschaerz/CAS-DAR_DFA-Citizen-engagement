3fa7341afdaa59baf209dd526cd420969e57a1b9
Jean-François Schärz <jef.schaerz@bluewin.ch>
2020-06-16 21:52:21 +0200

    Fix problem with LoginComponent and ngModel with Mathias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DummyPageComponent } from './dummy-page/dummy-page.component';
-import { LoginPageComponent } from './security/login-page/login-page.component';
-import { LogoutButtonComponent } from './security/logout-button/logout-button.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ApiTokenInterceptorService } from "./api/api-token-interceptor.service";
+import { SecurityModule } from './security/security.module';
 
 // import dans notre module
 @NgModule({
   // Declar COMPONENTS of modules
   declarations: [
     AppComponent,
-    DummyPageComponent,
-    LoginPageComponent,
-    LogoutButtonComponent
+    DummyPageComponent
   ],
   // Import other Angular MODULES
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, 
-    FormsModule
+    FormsModule, SecurityModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule} from '@angular/forms' ;
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
 
 @NgModule({
   declarations: [LoginPageComponent, LogoutButtonComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, FormsModule
+  ],
+  exports : [LoginPageComponent, LogoutButtonComponent]
 })
 export class SecurityModule { }
