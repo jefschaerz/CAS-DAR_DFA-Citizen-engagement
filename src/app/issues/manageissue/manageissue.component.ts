59299fb2220c480173ab077a3e02e898e7faff91
Jean-François Schärz <jef.schaerz@bluewin.ch>
2020-07-05 22:20:36 +0200

    Try to get issueTypes list by the API

--- src/app/issues/manageissue/manageissue.component.ts
+++ src/app/issues/manageissue/manageissue.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
+import { IssueTypeService } from '../../api/services/issue-type.service';
 import { Issue } from 'src/app/models/issue';
+import { IssueType } from 'src/app/models/issue-type';
 
 @Component({
   selector: 'app-manageissue',
@@ -11,11 +13,11 @@ import { Issue } from 'src/app/models/issue';
 export class ManageissueComponent implements OnInit {
   issues: Issue[];
   newissue: Issue;
+  issueTypes: IssueType[];
   newIssueError: boolean;
 
-  constructor(private router: Router) {
+  constructor(private issueTypeService: IssueTypeService, private router: Router) {
     this.newissue = new Issue();
-
   }
 
   ngOnInit(): void {
@@ -24,6 +26,22 @@ export class ManageissueComponent implements OnInit {
   goToAllIssues() {
     console.log('Move to see all issues')
     this.router.navigate(['/seeissues']);
+  }
+
+  getTypesOfIssue() {
+    // Ask service for the list of current type of issues defined
+    // Perform the add user request to the API.
+
+    //TODO : Save list os issue correctly in array !!
+    // Check Jokes code
+    this.issueTypeService.loadAllIssueTypes().subscribe(
+      receivedIssueTypes => console.log(receivedIssueTypes)
+      // receivedIssueTypes => this.issueTypes = receivedIssueTypes
+    );
+
+
+    console.warn(`Issue will be ask by the API`);
+    console.log("IssueTypes : ", this.issueTypes);
   }
 
   onSubmit(form: NgForm) {
