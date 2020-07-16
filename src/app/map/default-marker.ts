eb387c752e6ea12dfc2921f71bf12f37c8035b9d
Jean-François Schärz <jef.schaerz@bluewin.ch>
2020-07-16 23:07:39 +0200

    Add marker (green) on map to get coordinates in Add issue component

--- src/app/map/default-marker.ts
+++ src/app/map/default-marker.ts
@@ -10,3 +10,14 @@ export const defaultIcon: Icon<IconOptions> = icon({
     // The path to the image's shadow to display. Also a leaflet asset
     shadowUrl: 'leaflet/marker-shadow.png'
 });
+// Add colored markers from 
+// https://github.com/pointhi/leaflet-color-markers
+
+export const greenIcon = new Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { defaultIcon } from './default-marker';
-import { latLng, MapOptions, marker, Marker, Map, tileLayer } from 'leaflet';
+import { defaultIcon, greenIcon } from './default-marker';
+import { latLng, MapOptions, marker, Marker, Map, tileLayer, LeafletMouseEvent } from 'leaflet';
 import { IssueService } from '../api/services/issue.service';
 import { Issue } from 'src/app/models/issue';
 
@@ -14,6 +14,7 @@ export class MapComponent implements OnInit {
   mapMarkers: Marker[];
   mapOptions: MapOptions;
   issuesList: Issue[];
+  newMarker: Marker;
 
   constructor(private issueService: IssueService) {
     this.issuesList = [];
@@ -25,56 +26,65 @@ export class MapComponent implements OnInit {
         )
       ],
       zoom: 13,
+      // Renan BE
       center: latLng(47.126058, 6.942254)
     };
     this.mapMarkers = [];
-    //   marker([47.127058, 6.942254], { icon: defaultIcon }),
-    //   marker([47.128058, 6.942254], { icon: defaultIcon }),
-    //   marker([47.129058, 6.942254], { icon: defaultIcon })
-    // ];
   }
 
   onMapReady(map: Map) {
+    // Define this.map for futur use of this.map
     this.map = map;
+
+    this.map.on('moveend', () => {
+      const center = this.map.getCenter();
+      console.log(`Map moved to ${center.lng}, ${center.lat}`);
+    });
+
+    // Define position on map click
+    map.on('click', <LeafletMouseEvent>(event) => {
+      console.log(event.latlng);
+      this.newMarker = marker(event.latlng, { icon: greenIcon }).bindTooltip("New").addTo(map);
+    });
   }
 
   addIssuesMarkers() {
-    // https://stackblitz.com/edit/angular-ngx-leaflet-tests-3p74ln
     // Need to link with the issue service
-    // simply iterate over the array of markers from our data service
+    // simply iterate over the array of markers from our issue service
     // and add them to the map
     // Subscribe to get list of all issues
     this.issueService.loadAllIssues().subscribe({
       next: (result) => {
         this.issuesList = result;
         console.log("Issues loaded : ", this.issuesList);
-        console.log('Number or Issues :', this.issuesList.length)
       },
-      error: (error) => console.warn("Error", error),
+      error: (error) => console.warn("Error during issues loading", error),
       complete: () => {
-        console.log('Number or Issues in completed:', this.issuesList.length)
+        //console.log('Number or Issues in completed:', this.issuesList.length)
         this.refreshMarkers(this.map)
       }
     });
   }
 
   refreshMarkers(map: L.Map) {
-    console.log('Number or Issues in refreshMarkers:', this.issuesList.length);
+    console.log('Number of Issues in refreshMarkers:', this.issuesList.length);
+    //Clear current markers
+    this.mapMarkers = [];
     for (var i = 0; i < this.issuesList.length; i++) {
       this.mapMarkers.push(this.addNewMarkerFromIssue(this.issuesList[i], map));
     }
+    console.log('Number of Markers:', this.mapMarkers);
   }
 
   addNewMarkerFromIssue(oneIssue: Issue, map: L.Map): L.Marker {
     console.log('Issue received :', oneIssue.location.coordinates, '-', oneIssue.description);
     let oneMarker = marker(<L.LatLngExpression>(oneIssue.location.coordinates), { icon: defaultIcon }).bindTooltip(oneIssue.description);
     console.log('Add marker to the map..');
-    // DO not work correctly because this.map not defined!
     oneMarker.addTo(map);
     return oneMarker;
   }
 
   ngOnInit(): void {
-  }
 
+  }
 }
